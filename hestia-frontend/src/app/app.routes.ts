72d67d3e1861251a4af51374833f320bf56758ea
juanortega <dannyortega0627@gmail.com>
2025-10-21 09:35:46 -0500

    Se realliza la autenticación con firebase y proteccion de rutas

--- hestia-frontend/src/app/app.routes.ts
+++ hestia-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' } // redirige rutas inválidas
+];
